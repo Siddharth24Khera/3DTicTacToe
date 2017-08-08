dd5f230420cc64af3c5e604fba847c7ae9d8f0ea
Siddharth Khera <siddharth.khera66@gmail.com>
2017-08-08 23:08:46 +0530

    Added some printing moves functionality

--- Source.java
+++ Source.java
@@ -7,10 +7,16 @@ public class Source
 	private Intelligence intel;
 	private int firstPlayer; // 0 for computer 1 for human
 	private ArrayList<ArrayList<ArrayList<Integer>>> board; // 0 for unoccupied; 1 for X; 2 for O
+	private int computerWins;
+	private int playerWins;
+	private int gameStrategy;
 
 	public Source(){
 		intel = new Intelligence();
-		firstPlayer = 0;
+		firstPlayer = 2;
+		computerWins = 0;
+		playerWins = 0;
+		gameStrategy = 0;
 
 		board = new ArrayList<ArrayList<ArrayList<Integer>>>(3);
 		for(int i=0;i<3;i++){
@@ -57,13 +63,27 @@ public class Source
 	{
 		Source source = new Source();
 		source.getIntel().getMagicCube().print_cube();
-		System.out.println(source.getIntel().getMagicCube().get_not_sum_good_list());
-		System.out.println(source.getIntel().getMagicCube().getSum_bad_list());
-		System.out.println(source.getIntel().getMagicCube().getSum_good_list());
+//		System.out.println(source.getIntel().getMagicCube().get_not_sum_good_list());
+//		System.out.println(source.getIntel().getMagicCube().getSum_bad_list());
+//		System.out.println(source.getIntel().getMagicCube().getSum_good_list());
 		System.out.println("Welcome to 3DTicTacToe");
-		System.out.println("Enter 1 for playing first or 2 for playing second");
+		System.out.println();
+		System.out.println("The game comes with three winning strategies");
+		System.out.println("Strategy 1 : Generate a collinear line");
+		System.out.println("Strategy 2 : Generate two collinear lines");
+		System.out.println("Strategy 3 : Generate as many collinear line by the end of 20th move");
+		System.out.println("Enter 1 for strategy 1, 2 for strategy 2 and 3 for strategy 3");
 		Scanner sc = new Scanner(System.in);
+		source.gameStrategy = sc.nextInt();
+		if(source.gameStrategy<1 || source.gameStrategy>3){
+			System.out.println("Enter valid number");
+		}
+		System.out.println();
+		System.out.println("Enter 1 for playing first or 2 for playing second");
 		int input_num = sc.nextInt();
+		if(input_num<1 || input_num>2){
+			System.out.println("Enter valid number");
+		}
 		if(input_num == 1){
 			source.firstPlayer = 1;
 			System.out.println("You play X");
@@ -77,18 +97,87 @@ public class Source
 			move_count += 1;
 
 			if (source.getIntel().getGameStatus() == 1){
-				System.out.println("Computer Wins");
-				return;
+				source.computerWins += 1;
+				source.getIntel().setGameStatus(0);
+				System.out.println("Computer Wins " + source.computerWins +" time");
+
+			}
+
+
+			ArrayList<Integer> loss_position_list = source.getIntel().getList_of_possible_loss();
+			if(loss_position_list.size() != 0){
+				//System.out.println(loss_position_list);
+				for(int i = 0;i<loss_position_list.size();i++){
+					int j = loss_position_list.get(i);
+					ArrayList<Integer> coordinates = source.getIntel().getBoardCoordinates(j);
+					//System.out.println(source.firstPlayer);
+					//if(source.getIntel().getMagicSquareArray().get(j)){
+					if(source.board.get(coordinates.get(0)).get(coordinates.get(1)).get(coordinates.get(2)) == source.firstPlayer){
+						source.playerWins += 1;
+						loss_position_list.remove(i);
+						//System.out.println("Winning Line: "+);
+						System.out.println("You Win "+ source.playerWins +" time");
+					}
+					if(source.board.get(coordinates.get(0)).get(coordinates.get(1)).get(coordinates.get(2)) == 3 - source.firstPlayer){
+						loss_position_list.remove(i);
+					}
+				}
+			}
+
+			if(source.gameStrategy == 1){
+				if(source.computerWins == 1){
+					System.out.println("Computer is the True Winner");
+					return;
+				}
+				if(source.playerWins == 1){
+					System.out.println("You are the True Winner");
+					return;
+				}
+			}
+
+			if(source.gameStrategy == 2){
+				if(source.computerWins == 2){
+					System.out.println("Computer is the True Winner");
+					return;
+				}
+				if(source.playerWins == 2){
+					System.out.println("You are the True Winner");
+					return;
+				}
+			}
+
+			if(source.gameStrategy == 3){
+				if(move_count == 21){
+					System.out.println();
+					System.out.println("Twenty moves are over now");
+					System.out.println("Human won "+source.playerWins+" time");
+					System.out.println("Computer won "+source.computerWins+" time");
+					if(source.playerWins > source.computerWins){
+						System.out.println("You are the True Winner");
+						return;
+					}
+					else if(source.playerWins < source.computerWins){
+						System.out.println("Computer is the True Winner");
+						return;
+					}
+					else{
+						System.out.println("This game is a tie");
+						return;
+					}
+				}
 			}
 
 			if (source.getIntel().getGameStatus() == 2 || move_count == 28){
 				System.out.println("Game Tied");
 				return;
 			}
+			System.out.println();
+			System.out.println("Move Number: "+move_count);
+			System.out.println("Computer Score: "+source.computerWins);
+			System.out.println("Player Score: "+source.playerWins);
 
-			if((source.firstPlayer == 1 && move_count % 2 ==1) || (source.firstPlayer == 0 && move_count % 2 ==0)){
+			if((source.firstPlayer == 1 && move_count % 2 ==1) || (source.firstPlayer == 2 && move_count % 2 ==0)){
 				// Human Turn
-				System.out.println("");
 				System.out.println("Your Turn: Enter layer, row and column separated by space");
 				int lay_num = sc.nextInt() - 1;
 				int row_num = sc.nextInt() - 1;
@@ -101,6 +190,7 @@ public class Source
 					col_num = sc.nextInt() - 1;
 				}
 
+
 				int XorO = 0;
 				if (source.firstPlayer == 1)
 					XorO = 1;
@@ -111,13 +201,13 @@ public class Source
 				source.getIntel().append_player_moves(index_bool_list);
 				source.getIntel().set_magicSquareArray(index_bool_list);
 				source.print_board();
-				continue;
 			}
 
 			else{
-				// Computer's Turn
+				// Computer's Turnk
 
 				int move_index = source.getIntel().play_a_move();
+				//System.out.println(move_index);
 				source.getIntel().append_comp_moves(move_index);
 				ArrayList<Integer> layer_row_col = source.getIntel().getBoardCoordinates(move_index);
 
@@ -133,10 +223,10 @@ public class Source
 					XorO = 1;
 					System.out.println("Computer Plays X");
 				}
+				System.out.println("Position played: "+(lay_num+1)+' '+(row_num+1)+' '+(col_num+1));
 				source.board.get(lay_num).get(row_num).set(col_num,XorO);
 
 				source.print_board();
-				continue;
 			}
 		}
 
