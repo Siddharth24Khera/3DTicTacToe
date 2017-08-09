d25e65cba0b6197129d7dc826008086e47e1010a
Ankur Sharma <ankur.sharma127@gmail.com>
2017-08-09 22:36:07 +0530

    Comments

--- MagicCube.java
+++ MagicCube.java
@@ -27,7 +27,7 @@ public class MagicCube{
         this.fillCube(); //fills the cube
         sum_good_list = new ArrayList<ArrayList<Integer>>(); //generates a list of all tuples with sum 42, and are collinear
         sum_bad_list = new ArrayList<ArrayList<Integer>>(); //generates a list of all tuples which are not collinear with sum 42
-        not_sum_good_list = new ArrayList<ArrayList<Integer>>();
+        not_sum_good_list = new ArrayList<ArrayList<Integer>>();//list with collinear tuples and sum is not 42
         generateLists();
     }
 
@@ -35,34 +35,23 @@ public class MagicCube{
         return magic_cube;
     }
 
-    public ArrayList<ArrayList<Integer>> getSum_good_list(){return sum_good_list;}
+    public ArrayList<ArrayList<Integer>> getSum_good_list(){
+        return sum_good_list;
+    }
 
-    public ArrayList<ArrayList<Integer>> getSum_bad_list(){return sum_bad_list;}
+    public ArrayList<ArrayList<Integer>> getSum_bad_list(){
+        return sum_bad_list;
+    }
 
-    public ArrayList<ArrayList<Integer>> get_not_sum_good_list(){return not_sum_good_list;}
+    public ArrayList<ArrayList<Integer>> get_not_sum_good_list(){
+        return not_sum_good_list;
+    }
 
     public int getOrder(){
         return order;
     }
 
-//    public void print_cube(){
-//        System.out.println("Printing the three layers of the magic cube");
-//        System.out.println();
-//        // (i,j,k) indexing
-//        //i for layer ; j for row; k for column
-//        for(int i=0;i<order;i++){
-//            System.out.println("Layer "+ (i+1));
-//            for(int j=0;j<order;j++){
-//                for(int k=0;k<order;k++){
-//                    System.out.print(magic_cube.get(i).get(j).get(k)+" ");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println();
-//        }
-//    }
-
-    public void print_cube(){
+    public void print_cube(){ //prints all the 11 surfaces of the magic cube
         System.out.println("Printing the top three layers of the magic cube\n");
         for(int i=0;i<order;i++){
             System.out.println("Layer "+ (i+1));
@@ -119,8 +108,8 @@ public class MagicCube{
 
     }
 
-
-    private void fillCube(){
+    //fills the cube according to the algorithm
+    private void fillCube(){ 
         int n_row = order/2, n_col = order/2, n_ht = 0;
         for(int i =0; i < Math.pow(this.order,3);i++) {
             magic_cube.get(n_ht).get(n_row).set(n_col, i+1);
@@ -137,7 +126,8 @@ public class MagicCube{
             }
         }
     }
-
+    
+    //adjust method - sets the values in the range from 0 to n by incrementing or decremting correspondingly
     private int adjust(int order, int num){
         while(num<0)
             num+=order;
@@ -146,6 +136,7 @@ public class MagicCube{
         return num;
     }
 
+    //Checks collinearity using a 3D concept - direction cosines must be parallel
     private boolean check_collinearity(Tuple one, Tuple two, Tuple three){
         int x_1 = one.get_x() - three.get_x();
         int x_2 = one.get_y() - three.get_y();
@@ -159,7 +150,9 @@ public class MagicCube{
             return true;
         return false;
     }
-
+    
+    //geenrate all the three lists required
+    //maintained a hash table to get the coordinates and corresponding number on the cube in O(1) time
     private void generateLists(){
         for(int i=1;i<26;i++){
             for(int j=i+1;j<27;j++){
@@ -184,6 +177,7 @@ public class MagicCube{
     }//end of method
 }
 
+//tuple class that stores a tuple of 3 numbers
 class Tuple {
     int i;
     int j;
